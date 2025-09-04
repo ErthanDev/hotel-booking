2d9107de72134187f79ffd968b8b3e92a7653287
Mtri nèeeeeee <90810549+mtrine@users.noreply.github.com>
2025-09-04 17:34:12 +0700

    update: delete log debug

--- src/guard/ws-jwt.guard.ts
+++ src/guard/ws-jwt.guard.ts
@@ -13,15 +13,12 @@ export class WsJwtGuard implements CanActivate {
     ) { }
 
     canActivate(context: ExecutionContext): boolean {
-        this.logger.log('WsJwtGuard: Checking authentication for WebSocket connection');
         const client: Socket = context.switchToWs().getClient<Socket>();
         const token =
             client.handshake.auth?.token ||
             client.handshake.headers?.authorization?.replace(/^Bearer\s+/i, '') ||
             client.handshake.query?.token;
-        this.logger.debug(`Extracted token: ${token}`);
         if (!token) {
-            this.logger.error('Token not found in handshake');
             throw new AppException({
                 message: 'Token not found',
                 errorCode: 'TOKEN_NOT_FOUND',
@@ -30,13 +27,10 @@ export class WsJwtGuard implements CanActivate {
         }
 
         try {
-            this.logger.log(`Verifying token: ${token}`);
             const payload = this.jwtService.verify(token);
-            this.logger.log(`Token verified successfully for user: ${payload.email}`);
             (client as any).user = payload;
             return true;
         } catch {
-            this.logger.error('Token verification failed');
             throw new AppException({
                 message: 'Token invalid',
                 errorCode: 'TOKEN_INVALID',
